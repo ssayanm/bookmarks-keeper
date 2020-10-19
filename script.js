696cb17517fe4c4fd4572c4508c974f4746c2a43
Sayan Mukherjee <hello@sayanmukherjee.com>
2020-10-19 08:40:48 +0530

    updated mobile styling

--- script.js
+++ script.js
@@ -15,3 +15,4 @@ function showModal(){
 //modal event listneer
 modalShow.addEventListener('click', showModal);
 modalClose.addEventListener('click', () => modal.classList.remove('show-modal'))
+window.addEventListener("click", (e) => (e.target === modal ? modal.classList.remove('show-modal') : false))
