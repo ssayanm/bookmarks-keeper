28461f63c186b8ffd2dfef71bf3c8a07024fe398
Sayan Mukherjee <hello@sayanmukherjee.com>
2020-10-19 09:01:25 +0530

    adding form validation

--- script.js
+++ script.js
@@ -16,3 +16,30 @@ function showModal(){
 modalShow.addEventListener('click', showModal);
 modalClose.addEventListener('click', () => modal.classList.remove('show-modal'))
 window.addEventListener("click", (e) => (e.target === modal ? modal.classList.remove('show-modal') : false))
+
+//validate form
+function validate(nameValue, urlValue){
+    const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+    const regex = new RegExp(expression);
+    if(urlValue.match(regex)){
+        alert('match');
+    }
+    if(!urlValue.match(regex)){
+        alert('please provide a valid website address')
+        return false;
+    }
+}
+
+// handle data from Form
+function storeBookmark(e){
+    e.preventDefault();
+    const nameValue = websiteNameEl.value;
+    let urlValue = websiteUrlEl.value;
+    if(!urlValue.includes('http://', 'https://')){
+        urlValue = `https://${urlValue}`;
+    }
+    validate(nameValue, urlValue)
+}
+
+// event listener
+bookmarkForm.addEventListener('submit', storeBookmark);
