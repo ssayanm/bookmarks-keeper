a4f67a76cc0b1ef14b3cde307a258988fd2d7c58
Sayan Mukherjee <hello@sayanmukherjee.com>
2020-10-19 08:32:19 +0530

    modal opening and closing

---
+++ script.js
@@ -0,0 +1,17 @@
+const modal = document.getElementById('modal');
+const modalShow = document.getElementById('show-modal');
+const modalClose = document.getElementById('close-modal');
+const bookmarkForm = document.getElementById('bookmark-form');
+const websiteNameEl = document.getElementById('website-name');
+const websiteUrlEl = document.getElementById('website-url');
+const bookmarksContainer = document.getElementById('bookmarks-container');
+
+// show modal, focus on input
+function showModal(){
+    modal.classList.add('show-modal');
+    websiteNameEl.focus();
+}
+
+//modal event listneer
+modalShow.addEventListener('click', showModal);
+modalClose.addEventListener('click', () => modal.classList.remove('show-modal'))
